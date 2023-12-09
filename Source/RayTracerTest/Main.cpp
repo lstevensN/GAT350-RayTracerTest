f3d8909a928a7743984634245807b98d2d4bf658
lstevensN <lstevens@student.neumont.edu>
2023-12-08 23:55:08 -0700

    the final render

--- Source/RayTracerTest/Main.cpp
+++ Source/RayTracerTest/Main.cpp
@@ -13,14 +13,15 @@
 
 using namespace std;
 
-void InitScene03(Scene& scene, const Canvas& canvas);  // Cornell Room
+void InitScene03(Scene& scene, const Canvas& canvas);     // Cornell Room
+void InitSceneFinal(Scene& scene, const Canvas& canvas);  // Final Scene
 
 int main(int argc, char* argv[])
 {
 	const int width = 400;
 	const int height = 300;
-	const int samples = 20;
-	const int depth = 6;
+	const int samples = 1000;
+	const int depth = 25;
 
 	std::cout << "Hello World\n";
 
@@ -33,9 +34,9 @@ int main(int argc, char* argv[])
 	Canvas canvas(width, height, renderer);
 
 	float aspectRatio = canvas.GetSize().x / (float)canvas.GetSize().y;
-	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 0, 0.5f, 18 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 0, 1, 0 }, 20.0f, aspectRatio);
+	std::shared_ptr<Camera> camera = std::make_shared<Camera>(glm::vec3{ 10, 0, 18 }, glm::vec3{ 0, 5, -6 }, glm::vec3{ 0, 1, 0 }, 30.0f, aspectRatio);
 
-	Scene scene(glm::vec3{ 1.0f }, glm::vec3{ 0.5f, 0.7f, 1.0f }); // sky color could be set with the top and bottom color
+	Scene scene(glm::vec3{ 0 }, glm::vec3{ 0.5f, 0.7f, 1.0f }); // sky color could be set with the top and bottom color
 	scene.SetCamera(camera);
 
 	// create material
@@ -47,7 +48,7 @@ int main(int argc, char* argv[])
 	auto plane = std::make_unique<Plane>(glm::vec3{ 0, -4, 0 }, glm::vec3{ 0, 1, 0 }, oldMaterial);
 	scene.AddObject(std::move(plane));
 
-	InitScene03(scene, canvas);
+	InitSceneFinal(scene, canvas);
 
 	// render scene
 	canvas.Clear({ 0, 0, 0, 1 });
@@ -84,7 +85,43 @@ int main(int argc, char* argv[])
 
 void InitSceneFinal(Scene& scene, const Canvas& canvas)
 {
+	// central cube structure
+	auto mesh = std::make_unique<Mesh>(std::make_shared<Dielectric>(color3_t{ 1, 1, 1 }, 1));
+	mesh->Load("models/cube.obj", glm::vec3{ 0, 5, -6 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 4, 4, 4 });
+	scene.AddObject(std::move(mesh));
 
+	mesh = std::make_unique<Mesh>(std::make_shared<Emissive>(color3_t{ 1, 1, 1 }, 1));
+	mesh->Load("models/cube.obj", glm::vec3{ -1.5f, 2, -9.5f }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 3, 3, 3 });
+	scene.AddObject(std::move(mesh));
+
+	mesh = std::make_unique<Mesh>(std::make_shared<Dielectric>(color3_t{ 1, 1, 1 }, 1));
+	mesh->Load("models/cube.obj", glm::vec3{ -2.5f, -0.5f, -12 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 2, 2, 2 });
+	scene.AddObject(std::move(mesh));
+
+
+	// moon
+	auto sphere = std::make_unique<Sphere>(glm::vec3{ 7.5f, 15, -18 }, 2.0f, std::make_shared<Emissive>(color3_t{ 1, 1, 1 }, 1));
+	scene.AddObject(std::move(sphere));
+
+
+	// background buildings
+	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 0, 0, 0 }));
+	mesh->Load("models/cube.obj", glm::vec3{ -30, 0, -45 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 17, 58, 17 });
+	scene.AddObject(std::move(mesh));
+
+	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 0, 0, 0 }));
+	mesh->Load("models/cube.obj", glm::vec3{ -10, 0, -45 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 17, 58, 17 });
+	scene.AddObject(std::move(mesh));
+
+
+	// buildings
+	mesh = std::make_unique<Mesh>(std::make_shared<Metal>(color3_t{ 0.5f, 0.5f, 0.5f }, 0.5f));
+	mesh->Load("models/cube.obj", glm::vec3{ -10, 0, -25 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 10, 38, 10 });
+	scene.AddObject(std::move(mesh));
+
+	mesh = std::make_unique<Mesh>(std::make_shared<Lambertian>(color3_t{ 0.5f, 0.5f, 0.5f }));
+	mesh->Load("models/cube.obj", glm::vec3{ 2, 0, -30 }, glm::vec3{ 0, 0, 0 }, glm::vec3{ 8, 28, 8 });
+	scene.AddObject(std::move(mesh));
 }
 
 void InitScene03(Scene& scene, const Canvas& canvas)
